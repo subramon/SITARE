af4a24fa4814b681d1ec289438f4e07838960a0b
Ramesh Subramonian <ramesh.subramonian@target.com>
2024-12-13 02:42:02 +0000

    ok

---
+++ scramble/canonical_form/inc/canonicalize.h
@@ -0,0 +1,13 @@
+#define MAX_LEN_CANONICAL_STR 32
+#define MAX_LEN_STR 32
+int
+canonicalize(
+    const char * const in_str, // input 
+    uint8_t cstr[MAX_LEN_CANONICAL_STR]
+    )
+{
+  int status = 0;
+  uint8_t count[NUM_ALPAHBET];
+BYE:
+  return status;
+}
---
+++ scramble/canonical_form/src/canonicalize.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "canonicalize.h"
+int
+canonicalize(
+    const char * const str, // input 
+    char cstr // output 
+    )
+{
+  int status = 0;
+BYE:
+  return status;
+}
